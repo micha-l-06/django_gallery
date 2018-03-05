c8d0643ebfbca4350d23c34332e6154510af03ec
Michal Boljesik <michal@10.0.2.15>
2018-03-05 18:35:17 +0100

    A model representing a picture has been created.

--- django_gallery/gallery_app/models.py
+++ django_gallery/gallery_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Picture(models.Model):
+    # change to picture type
+    picture = models.CharField()
+    # do I need this one ?
+    name = models.CharField()
+    # do I need this one ? change to bool if so
+    grayscale = picture = models.CharField()
